9259e101684cf3f5c5e72fb13dc08745257aa93d
Maanibhadra Digital -bharuch <sgsbackup1@gmail.com>
2019-03-28 18:18:03 +0530

    Add files via upload

---
+++ firebaeUI/config.js
@@ -0,0 +1,13 @@
+var config = {
+  apiKey: "AIzaSyDNRtpa86v986mO3pePPursUW1XtahmrDo",
+    authDomain: "gfem-b7b57.firebaseapp.com",
+    databaseURL: "https://gfem-b7b57.firebaseio.com",
+    projectId: "gfem-b7b57",
+    storageBucket: "gfem-b7b57.appspot.com",
+    messagingSenderId: "144283479484"
+};
+firebase.initializeApp(config);
+
+// Google OAuth Client ID, needed to support One-tap sign-up.
+// Set to null if One-tap sign-up is not supported.
+var CLIENT_ID = null;
